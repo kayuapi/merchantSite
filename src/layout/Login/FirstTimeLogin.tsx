4b5826c36a8342a23e012b8ff95bea5a2003a261
cong <zhicong.tay@gmail.com>
2020-09-01 13:10:13 +0800

    price in product display layout bug fixed

--- src/layout/Login/FirstTimeLogin.tsx
+++ src/layout/Login/FirstTimeLogin.tsx
@@ -205,7 +205,7 @@ const Login = () => {
                             <div className={classes.hint}>
                                 CHM Ordering System
                                 <br />
-                                v 1.0.5
+                                v 2.0.6
                                 <br />
                             </div>
                             <div className={classes.form}>
