1e4a7770c4be76ba7c7d818505404e083979c961
cong <zhicong.tay@gmail.com>
2020-11-11 09:49:40 +0800

    version 2.0.14

--- src/layout/Login/FirstTimeLogin.tsx
+++ src/layout/Login/FirstTimeLogin.tsx
@@ -205,7 +205,7 @@ const Login = () => {
                             <div className={classes.hint}>
                                 CHM Ordering System
                                 <br />
-                                v 2.0.13
+                                v 2.0.14
                                 <br />
                             </div>
                             <div className={classes.form}>
