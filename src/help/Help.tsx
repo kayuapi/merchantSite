5f1460da05286419be0e2921a7f31e2bbed0fca4
cong <zhicong.tay@gmail.com>
2021-02-02 21:23:03 +0800

    2.3.1: bug fixed

--- src/help/Help.tsx
+++ src/help/Help.tsx
@@ -71,7 +71,8 @@ const Help = () => {
                     <Box maxWidth="40em">
                       <Typography variant="body1" component="ul" style={{color: 'white'}} gutterBottom>
                         <li>NEW FEATURE: enable basic looping notification feature</li>
-                        <li>FIX BUGS: fix duplicated a la carte menu items with variants id bug</li>
+                        <li>NEW FEATURE: auto snap to the adjacent y position when dragging near to it</li>
+                        <li>FIX BUGS: fix duplicated a la carte menu items with variants id bug, add new item position bug fixed, category tabs icon changed.</li>
                       </Typography>
                     </Box>
                 </Box>
